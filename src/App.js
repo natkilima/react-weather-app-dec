8073a8bb1ef0b22f53fe56c6ae0d1ae62d3ce4d8
natkilima <nathalie.rognon.juyn@gmail.com>
2021-12-02 13:55:56 +0000

    Added the GitHub repo url

--- src/App.js
+++ src/App.js
@@ -2,5 +2,27 @@ import "./App.css";
 import React from "react";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App </h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://eloquent-elion-c58bd0.netlify.app/index.html"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Nathalie Rognon{" "}
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/natkilima/react-weather-app-dec"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
