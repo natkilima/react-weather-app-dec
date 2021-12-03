cc39f8a343dfa566d4ab4cedf60ef729e40251ac
natkilima <nathalie.rognon.juyn@gmail.com>
2021-12-03 09:46:22 +0000

    Bootstrap integrated and HTML created in Weather Component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
 import "./App.css";
 import React from "react";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App </h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://eloquent-elion-c58bd0.netlify.app/index.html"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Nathalie Rognon{" "}
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/natkilima/react-weather-app-dec"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App </h1>
+        <Weather />
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://eloquent-elion-c58bd0.netlify.app/index.html"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Nathalie Rognon{" "}
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/natkilima/react-weather-app-dec"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
